b52f8caff333616e9f7185a24b6df8ebe2b59317
Kyler <mbqsniper@gmail.com>
2014-03-30 23:44:38 -0700

    Update Stock_Portfolio.py

--- Stock_Portfolio.py
+++ Stock_Portfolio.py
@@ -3,50 +3,50 @@ prices = {}
 exposure = {}
 
 
+
 def AddName():
-    stock_symbol = input('Enter Company Symbol: ')
-    company_name = input('Enter Company Name: ')
-    names[stock_symbol] = company_name
+    stock_symbol = input("Enter Company Symbol: ")
+    company_name = input("Enter Company Name: ")
+    names[stock_symbol] =  company_name
     return stock_symbol
 
 
+
+ 
 def AddPrices(stock_symbol):
-    buy_price = float(input('Enter the Buy price: '))
-    current_price = float(input('Enter the Current Price: '))
+    buy_price = float(input("Enter the Buy price: "))
+    current_price = float(input("Enter the Current Price: "))
+    
 
     prices[stock_symbol] = (buy_price, current_price)
 
 
 def AddExposure(stock_symbol):
-    risk = float(input('Enter the Risk: '))
-    shares = float(input('Enter the number of Shares: '))
+    risk = float(input("Enter the Risk: "))
+    shares = float(input("Enter the number of Shares: "))
 
     exposure[stock_symbol] = (risk, shares)
-
 
 def AddStock():
     stock_symbol = AddName()
     AddPrices(stock_symbol)
     AddExposure(stock_symbol)
 
-
 def GetSale(stock_symbol):
+    
+    ESV = ((prices[stock_symbol][0]-prices[stock_symbol][1]) - exposure[stock_symbol][0]*prices[stock_symbol][0])*exposure[stock_symbol][1]
 
-    ESV = (prices[stock_symbol][0] - prices[stock_symbol][1]
-           - exposure[stock_symbol][0] * prices[stock_symbol][0]) \
-        * exposure[stock_symbol][1]
-
-    return (stock_symbol, ESV)
+    return stock_symbol, ESV
 
 
 def Main():
-    print '1. Add Stock'
-    print '2. Recommend Sale'
-    print '3. Exit'
+    print("1. Add Stock")
+    print("2. Recommend Sale")
+    print("3. Exit")
     menuChoice = 0
 
     while menuChoice != 3:
-        menuChoice = int(input('What Would You Like To Do? '))
+        menuChoice = int(input("What Would You Like To Do? "))
         if menuChoice == 1:
             menuChoice = 0
             AddStock()
@@ -55,7 +55,3 @@ def Main():
             GetSale()
         elif menuChoice != 3:
             Main()
-
-
-
-			
