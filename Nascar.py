8dd284be64a26cc8760c3d1f0cfef6bc579734fa
Kyler <mbqsniper@gmail.com>
2014-04-02 10:10:57 -0700

    2

--- Nascar.py
+++ Nascar.py
@@ -1,3 +1,37 @@
 class Car():
     def __init__(self):
-        
+        self.total_odometer_miles = 0.0
+        self.speed_in_miles_per_hour = random.randint(1, 120)
+        self.driver_name = ""
+        self.sponsor_name = ""
+
+drivers_names = {
+            "Al",
+            "Bob",
+            "Carl",
+            "Dan",
+            "Ed",
+            "Fred",
+            "George",
+            "Hank",
+            "Ike",
+            "Jeff",
+            "Kelley",
+            "Lou",
+            "Niko"
+        }
+            
+
+def get_list_of_cars(how_many):
+    cars = []
+
+    number_of_cars = 0
+
+    while number_of_cars > 20:
+        cars.append(Car[])
+        number_of_cars += 1
+
+    return cars
+
+def assign_drivers(cars):
+    
