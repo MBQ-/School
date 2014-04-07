12a2c59ae5c7daadec37b0f63f0dd58f78a05d72
Kyler <mbqsniper@gmail.com>
2014-04-07 10:14:48 -0700

    wip

---
+++ Address.py
@@ -0,0 +1,34 @@
+class Address():
+    def __init__(self):
+        self.street = ""
+        self.city = ""
+        sekf.staate = ""
+        self.zip = ""
+
+    def get_street(self):
+        return self.street
+
+    def set_street(self, new_street):
+        self.street = new_street
+
+    def get_city(self):
+        return self.city
+
+    def set_city(self, new_city):
+        self.city = new_city
+
+    def get_state(self):
+        return self.state
+
+    def set_state(self, new_state):
+        self.statet = new_state
+
+    def get_zip(self):
+        return self.zip
+
+    def set_zip(self, new_zip):
+        self.zip = new_zip
+
+class Person():
+    def __init__(self):
+        
